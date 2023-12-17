c68e37aa117a560086edad7ef5230ca0c63e9044
pambaka <pambaka@ya.ru>
2023-12-18 00:18:45 +0100

    feat: queue is implemented

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  listNode;
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.listNode;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if (!this.listNode) {
+      this.listNode = new ListNode(value);
+    } else {
+      let currentNode = this.listNode;
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = new ListNode(value);
+    }
+    
+    return this.listNode;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const dequeuedNodeValue = this.listNode.value;
+    this.listNode.value = this.listNode.next.value;
+    this.listNode.next = this.listNode.next.next;
+    return dequeuedNodeValue;
   }
 }
 
